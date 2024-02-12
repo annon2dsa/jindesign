316bbaa9b24a819dcd215adde966d25a20240a14
Annon Dsa <annonpd@gmail.com>
2024-02-12 15:29:28 +0530

    animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import { Button, Typography, AppBar, SwipeableDrawer } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
 
 const HeadComponent = styled("div")(({ theme }) => ({
   display: "flex",
@@ -18,6 +19,22 @@ const HeadComponent = styled("div")(({ theme }) => ({
 const LogoComponent = styled("img")(({ theme }) => ({
   maxWidth: "6.313rem",
   maxHeight: "3.125rem",
+  background: "#333",
+  transition: "transform 0.3s ease-in-out", // Adding a transition for a smoother effect
+  position: "absolute",
+  ":hover": {
+    transform: "translateX(125px)", // You can adjust the value based on your preference
+  },
+}));
+const ImgComponent = styled("img")(({ theme }) => ({
+  maxWidth: "6.313rem",
+  maxHeight: "3.125rem",
+  transition: "transform 0.3s ease-in-out", // Adding a transition for a smoother effect
+  top: 0,
+  // position: "relative",
+  // ":hover": {
+  //   transform: "scale(1.1)",
+  // },
 }));
 
 const TabHead = styled("div")(({ theme }) => ({
@@ -31,16 +48,17 @@ const TabHead = styled("div")(({ theme }) => ({
   },
 }));
 
-const TabComponent = styled(Button)(({ theme }) => ({
-  color: "#fff",
-}));
-
 const Text = styled(Typography)(({ theme }) => ({
   font: "syne",
   fontWeight: 600,
   fontSize: "16px",
   lineHeight: "normal",
   textDecoration: "none",
+  ":hover": {
+    color: "#8ED6FF",
+    transition: "font-size 0.5s, color 0.3s",
+    fontSize: "18px",
+  },
 }));
 
 const Header = () => {
@@ -49,7 +67,6 @@ const Header = () => {
 
   React.useEffect(() => {
     /* First let's define an event listener */
-
     const handleScroll = () => {
       if (window.scrollY < scrollY) {
         setNegativeScroll(true);
@@ -75,12 +92,15 @@ const Header = () => {
       <HeadComponent sx={{ backdropFilter: scrollY !== 0 ? "blur(1rem)" : "" }}>
         <Link to="/">
           <LogoComponent src="/images/logo/jim.svg" alt="logo" />
+          <ImgComponent src="/images/logo/kitta.svg" alt="kittalogo" />
         </Link>
         <TabHead>
           <Link
             style={{
               textDecoration: "none",
               color: "#FFFFFF",
+              // "&:hover": { color: "#8ED6FF" },
+              transition: "color 0.3s",
             }}
             to="/about"
           >
--- src/components/page-section/HomePage/sub-components/SectionTwo.tsx
+++ src/components/page-section/HomePage/sub-components/SectionTwo.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { styled } from "@mui/system";
+import { keyframes, styled } from "@mui/system";
 import { Typography, IconButton, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
+
 const MainContainer = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
@@ -39,22 +39,6 @@ const SubText = styled(Typography)(({ theme }) => ({
   },
 }));
 
-const MainText = styled(Typography)(({ theme }) => ({
-  fontWeight: 500,
-  fontSize: "3rem",
-  lineHeight: "3.65rem",
-  color: "#FFFFFF",
-  textAlign: "left",
-  paddingLeft: "5rem",
-  maxWidth: "49.688rem",
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "1.25rem",
-    lineHeight: "140%",
-    fontWeight: 700,
-    paddingLeft: "0rem",
-  },
-}));
-
 const AboutContainer = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
@@ -68,6 +52,57 @@ const AboutContainer = styled("div")(({ theme }) => ({
   },
 }));
 
+const quackAnimation = keyframes`
+  0%, 100% {
+    transform: translateX(0);
+  }
+  10%, 30%, 50%, 70%, 90% {
+    transform: translateX(-5px);
+  }
+  20%, 40%, 60%, 80% {
+    transform: translateX(5px);
+  }
+`;
+
+const bounceAnimation = keyframes`
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
+  }
+  10% {
+    transform: translateY(-10px);
+  }
+  20% {
+    transform: translateY(-15px);
+  }
+  30% {
+    transform: translateY(-10px);
+  }
+  40% {
+    transform: translateY(-15px);
+  }
+  40% {
+    transform: translateY(-10px);
+  }
+  50% {
+    transform: translateY(-px);
+  }
+  60% {
+    transform: translateY(-10px);
+  }
+  70% {
+    transform: translateY(-15px);
+  }
+  80% {
+    transform: translateY(-10px);
+  }
+  90% {
+    transform: translateY(-15px);
+  }
+  100% {
+    transform: translateY(-10px);
+  }
+`;
+
 const ButtonContainer = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
@@ -76,15 +111,20 @@ const ButtonContainer = styled("div")(({ theme }) => ({
   // width: "100%",
   paddingLeft: "5rem",
   [theme.breakpoints.down("sm")]: { paddingLeft: "0rem" },
+  transition: "transform 0.3s ease-in-out",
+  ":hover": {
+    // transform: "scale(1.1)",
+    animation: `${quackAnimation} 0.6s infinite`,
+  },
 }));
 
-const AboutIcon = styled(motion.button)(({ theme }) => ({
+const AboutIcon = styled(IconButton)(({ theme }) => ({
   height: "14.438rem",
   width: "14.438rem",
   background: "#8ED6FF",
-  borderRadius: "50%",
-  // box-shadow: 0 0px 10px var(--cfOrange);
+  transition: "transform 0.3s ease-in-out",
   ":hover": {
+    transform: "scale(1.5)",
     background: "#8ED6FF",
     boxShadow: "0px 0px 10px #8ED6FF",
   },
@@ -103,6 +143,52 @@ const AboutButtonContainer = styled("div")(({ theme }) => ({
 const Image = styled("img")(({ theme }) => ({
   [theme.breakpoints.down("sm")]: {
     maxWidth: "353px",
+  },
+}));
+
+const colorChangeAnimation = keyframes`
+  0% {
+    color: #8ED6FF;
+  }
+  50% {
+    color: #FFA500;  // Change to your desired color
+  }
+  100% {
+    color: #8ED6FF;
+  }
+`;
+
+const colorAmination = keyframes`
+ 0% {
+    color: ;
+  }
+  50% {
+    color: #8ED6FF;  // Change to your desired color
+  }
+  100% {
+    color: #FFFFF;
+  }
+`;
+
+const TextSpan = styled("span")(({ theme }) => ({
+  animation: `${colorChangeAnimation} 5s infinite`, // Adjust the duration as needed
+  transition: "transform 0.3s ease-in-out",
+}));
+
+const MainText = styled(Typography)(({ theme }) => ({
+  fontWeight: 500,
+  fontSize: "3rem",
+  lineHeight: "3.65rem",
+  color: "#FFFFFF",
+  textAlign: "left",
+  paddingLeft: "5rem",
+  maxWidth: "49.688rem",
+  animation: `${colorAmination} 5s`,
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1.25rem",
+    lineHeight: "140%",
+    fontWeight: 700,
+    paddingLeft: "0rem",
   },
 }));
 
@@ -137,33 +223,22 @@ const SectionTwo = () => {
         >
           <MainText>
             Design isn't just my job;
-            <br /> it's my <span style={{ color: "#8ED6FF" }}>
-              passion.
-            </span>{" "}
-            I'm always on the
+            <br /> it's my <TextSpan>passion.</TextSpan> I'm always on the
             <br />
             lookout for exciting new projects <br />
             and creative challenges.
           </MainText>
           <AboutButtonContainer>
-            {/* <motion.button
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            > */}
-            <AboutIcon
-              onClick={handleClick}
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-            >
+            <AboutIcon onClick={handleClick}>
               <SubText>About Me :)</SubText>
             </AboutIcon>
-            {/* </motion.button> */}
           </AboutButtonContainer>
         </TextContainer>
         <ButtonContainer>
           <Button
             sx={{
               width: "12.563rem",
+              height: "54px",
               borderRadius: "5.563rem",
               textTransform: "none",
             }}
